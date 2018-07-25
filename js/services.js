2292b9a93bdd3d414f98bd996fd5a0dee42e7cfc
Jatin Kumar <jatn15011999@gmail.com>
2018-07-26 05:08:21 +0530

    services section made responsive

--- js/services.js
+++ js/services.js
@@ -173,7 +173,7 @@ $(document).ready(function() {
               .css({
                 left: randLeft,
                 top: randTop,
-                color: "#F9F7F6",
+                color: "#eee",//F9F7F6
                 zIndex: "initial"
               });
           }
